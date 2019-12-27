5ed06b3eff15a7a02b973651b9053de605fddf01
philip <philip.rose@haspa-next.de>
2019-12-27 10:46:24 +0100

    init commit

---
+++ app/src/main/java/com/prose/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.prose.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
